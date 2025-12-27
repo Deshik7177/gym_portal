0fdb1ba23b375c5f1838eda2bcdd662d83e35990
Deshik Paila <pdhanadeshik7177@gmail.com>
2025-12-27 13:03:16 +0000

    so there is so much space between the navbar and starting of the page in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function LandingPage() {
         </nav>
       </header>
       <main className="flex-1">
-        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+        <section className="w-full py-12 md:py-20 lg:py-24">
           <div className="container px-4 md:px-6">
             <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
               {heroImage && (
@@ -186,5 +186,3 @@ export default function LandingPage() {
     </div>
   );
 }
-
-    
