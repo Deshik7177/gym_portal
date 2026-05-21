0dccea0bb42219ad211be58a9bfaff29b9922511
Deshik Paila <pdhanadeshik7177@gmail.com>
2026-05-21 07:31:43 +0000

    how to connect to firebase

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -16,4 +16,5 @@ export function initializeFirebase() {
 export * from './provider';
 export * from './client-provider';
 export * from './auth/use-user';
-// Re-export hooks as they are added
+export * from './firestore/use-collection';
+export * from './firestore/use-doc';
