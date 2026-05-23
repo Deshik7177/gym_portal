baa88871edfa784a13070c752e32be520c25dde0
Deshik Paila <pdhanadeshik7177@gmail.com>
2026-05-23 08:13:23 +0000

    for every month the calender box size is changing i need it regid

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -18,20 +18,21 @@ function Calendar({
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
+      fixedWeeks
       className={cn("p-3", className)}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
-        month_caption: "flex justify-center pt-1 relative items-center h-9 mb-8",
+        month_caption: "flex justify-center pt-1 relative items-center h-7 mb-8",
         caption_label: "text-sm font-bold tracking-tight",
         nav: "flex items-center",
         button_previous: cn(
           buttonVariants({ variant: "outline" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute left-1 top-1 flex items-center justify-center transition-all z-10"
+          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute left-1 top-0 flex items-center justify-center transition-all z-10"
         ),
         button_next: cn(
           buttonVariants({ variant: "outline" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute right-1 top-1 flex items-center justify-center transition-all z-10"
+          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute right-1 top-0 flex items-center justify-center transition-all z-10"
         ),
         month_grid: "w-full border-collapse",
         weekdays: "flex w-full mb-1",
