703128d638d0e75f12113d7cd8da190d3647a87f
Deshik Paila <pdhanadeshik7177@gmail.com>
2026-05-29 07:08:36 +0000

    2page-ac91d81e02d5c662.js:1 Uncaught ReferenceError: Badge is not define

--- src/app/admin/register/page.tsx
+++ src/app/admin/register/page.tsx
@@ -57,6 +57,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Badge } from '@/components/ui/badge';
 
 function RegisterForm() {
   const { toast } = useToast();
